05b172490e9ec5b57323c0384b86c7812663d01d
Andrej T. <andrejtrajchevski@gmail.com>
2015-09-26 18:05:04 +0200

    whitespace

--- 3.hs
+++ 3.hs
@@ -8,13 +8,8 @@ primes = filter isPrime [2 ..]
 
 largestFactor (x:xs) 1 = x
 largestFactor (x:xs) n =
-	if
-		n `mod` x == 0
-	then
-		largestFactor (x:xs) (n `div` x)
-	else
-		largestFactor xs n
+	if n `mod` x == 0
+	then largestFactor (x:xs) (n `div` x)
+	else largestFactor xs n
 
-
-main =
-	print $ largestFactor primes 600851475143
+main =	print $ largestFactor primes 600851475143
