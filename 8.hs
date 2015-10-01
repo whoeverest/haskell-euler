7a35681400a2a15b631556d0e4582ea2225edcca
Kiril Vangelovski <k.vangelovski@gmail.com>
2015-10-01 21:38:24 +0200

    8ma

---
+++ 8.hs
@@ -0,0 +1,21 @@
+import Data.Char
+
+
+convertToInt :: String -> [Int]
+convertToInt l = map digitToInt l
+
+largestAdjProd :: [Int] -> Int -> Int -> Int
+largestAdjProd l maxProd adj
+    | length (take adj l) < adj = maxProd
+    | otherwise =
+        let
+            listProduct l = foldl (*) 1 l
+            product = listProduct (take adj l)
+            newMax = max product maxProd
+        in
+            largestAdjProd (tail l) newMax adj
+    
+main :: IO ()
+main = do
+    number <- readFile "big.txt"
+    print $ largestAdjProd (convertToInt number) 0 13
